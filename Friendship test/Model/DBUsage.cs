0fa3ee63369764915f5b71a93033db204c690e88
Alekseeva Elizaveta <liza250219999@mail.ru>
2017-11-29 19:22:53 +0300

    DBUsage

---
+++ Friendship test/Model/DBUsage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class DBUsage
+    {
+
+        FriendTestEntities1 db = new FriendTestEntities1();
+
+        public List<Person> ShowPerson()
+        {
+            List<Person> people = new List<Person>();
+            people = db.Person.ToList();
+
+            return people;
+        }
+
+        public List<Question> ShowQuestions()
+        {
+            List<Question> questions = new List<Question>();
+            questions = db.Question.ToList();
+
+            return questions;
+        }
+
+        public void AddPerson(Person per)
+        {
+            db.Person.Add(per);
+            db.SaveChanges();
+        }
+    }
+}
