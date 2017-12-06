3eaf5287021beb32ee881113815f22821e0abd72
sevam <sevazubkov@gmail.com>
2017-12-06 19:48:27 +0300

    VK Parser

---
+++ Friendship test/ClassesLibrary/Classes/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ClassesLibrary.Classes
+{
+    class Response
+    {
+        [JsonProperty("response")]
+        public List<User> Users { get; set; }
+    }
+}
---
+++ Friendship test/ClassesLibrary/Classes/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ClassesLibrary.Classes
+{
+    class User
+    {
+        [JsonProperty("uid")]
+        public int Id { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("photo_100")]
+        public string PhotoUrl { get; set; }
+    }
+}
