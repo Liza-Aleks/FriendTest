67de04d1d8a687b844f541655298680f7b9922e2
sevam <sevazubkov@gmail.com>
2017-12-06 17:17:53 +0300

    VK class added

---
+++ Friendship test/ClassesLibrary/Hashing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Hashing
+    {
+        private const int base_ = 2;
+        private const int mod = 1000000007;
+        public static int makeHash(string password)
+        {
+            int ans = 0;
+            int p = 1;
+            for (int i = 0; i < password.Length; i++)
+            {
+                int x = password[i];
+                ans = (ans + p * x) % mod;
+                p = (p * base_) % mod;
+            }
+            return ans;
+        }
+    }
+}
